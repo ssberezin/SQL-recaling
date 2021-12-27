95896d2d1b858be75964d8b760a35d5ba3cfdac6
ssberezin <45270437+ssberezin@users.noreply.github.com>
2021-12-27 14:56:49 +0200

    27.12.21 -lab2

--- laba_2.sql
+++ laba_2.sql
@@ -1,5 +1,6 @@
 --Для базы данных издательства написать следующие запросы:
 --1. Вывести информацию обо всех книгах, в имени которых больше 4-х слов.
+use Publishing
 With tmpTable as 
 (
 SELECT *
@@ -30,45 +31,113 @@ From Books b
 Select * From #AveragePrise
 --4. Вывести информацию о книгах по «Computer Science» с наибольшим
 --количеством страниц.
---https://stackoverflow.com/questions/612231/how-can-i-select-rows-with-maxcolumn-value-partition-by-another-column-in-mys
-With tmpTable2 (BookName , Theme, Pages ) as 
+
+With tmpTable as 
 (
-SELECT b.*, t.NameTheme
-FROM BOOKs b
-join Themes t on t.Id_Theme=b.Id_Theme
-join 
-(Select b.NameBook,  MAX(b.Pages) as Maxpages
-from books b
-join Themes t on b.Id_Theme=t.Id_Theme 
-Group by b.NameBook
-) groupedb
-on b.NameBook = groupedb.NameBook
- and b.Pages=groupedb.Maxpages
-
-
-
-
---Group by b.NameBook,t.NameTheme, b.Pages
+Select b.NameBook as 'Название книги', b.Pages as 'Наибольшее количество странитц', t.NameTheme
+From Books b
+join  Themes t  on b.Id_Theme = t.Id_Theme
+where t.NameTheme='Детектив' and b.Pages = 
+(SELECT MAX(b.Pages)
+FROM Books b
+join  Themes t  on b.Id_Theme = t.Id_Theme
+where t.NameTheme='Детектив'
+)
+Group by b.NameBook,b.Pages, t.NameTheme
 )
 
+select * from tmpTable
 
+-- тут найдены книги с максималбным кол-волм страниц по каждому жанру 
 
-select * from tmpTable2
+With tmpTable  as 
+(
+Select b.NameBook as 'Название книги', b.Pages as 'Наибольшее количество странитц', t.NameTheme
+From Books b
+join  Themes t  on b.Id_Theme = t.Id_Theme
+join
+(SELECT MAX(b.Pages) as pages, t.NameTheme
+FROM Books b
+join  Themes t  on b.Id_Theme = t.Id_Theme
+Group by t.NameTheme) groupedb
+on b.Pages=groupedb.pages and t.NameTheme=groupedb.NameTheme
+
+Group by   b.Pages  , t.NameTheme, b.NameBook
+)
+
+select * from tmpTable
 
 --5. Показать авторов и самую старую книгу по каждому из них. Результат
 --сохранить в глобальную временную таблицу.
+
+create table ##AuthorBooks (BookName nvarchar(30),Author nvarchar (30), DateOfPublishing date )
+
+insert into ##AuthorBooks
+Select b.NameBook as 'Книга',a.FirstName+' ' +a.LastName  as 'Автор',  b.DateOfPublish as 'Дата публикации'
+From Books b
+join Authors a on b.Id_Author=a.Id_Author
+join (Select a.FirstName+' ' +a.LastName  as 'Author',  MIN (b.DateOfPublish) as 'DateOfPublishing'
+From Books b
+join Authors a on b.Id_Author=a.Id_Author
+Group by a.FirstName+' ' +a.LastName 
+) groupeda
+on b.DateOfPublish=groupeda.DateOfPublishing
+and a.FirstName+' '+a.LastName=groupeda.Author
+Group by  b.NameBook,a.FirstName+' ' +a.LastName,b.DateOfPublish 
+
+Select * from ##AuthorBooks
+
 --6. Показать на экран сумму страниц по каждой из тематик, при этом
 --учитывать только книги одной из следующих тематик: «Computer
 --Science», «Science Fiction», «Web Technologies» и с количеством страниц
 --более 300.
+With tmpTable  as 
+(
+SELECT SUM(b.Pages) SumPages,t.NameTheme Theme
+FROM Themes t 
+join Books b on b.Id_Theme=t.Id_Theme
+Where t.NameTheme in ('Детектив' ,'Проза')
+Group by t.NameTheme
+having sum(b.Pages)>300
+)
+select * from tmpTable
+
 --7. Найти авторов, живущих в тех странах, где есть хотя бы один из
 --магазинов по распространению книг, занесенных в БД. Результат запроса
 --поместить в отдельное представление.
+CREATE VIEW AuthorsCountriesShops (AuthorName, Country,Shop) as
+Select a.FirstName+' '+a.LastName Author, c.NameCountry Country, sh.NameShop
+From Authors a
+join Country c on c.Id_Country=a.Id_Country
+join Shops sh on sh.Id_Country=c.Id_Country
+Group by a.FirstName+' '+a.LastName, c.NameCountry, sh.NameShop
+
+Select * From AuthorsCountriesShops
+
 --8. Написать представление, содержащее самую дорогую книгу тематики,
 --например, «Web Technologies».
+
+CREATE VIEW AuthorMostExpansiveBooks (BookName ,Author ,Price ) as
+Select b.NameBook as 'Книга',a.FirstName+' ' +a.LastName  as 'Автор',  b.Price as 'Цена'
+From Books b
+join Authors a on b.Id_Author=a.Id_Author
+join (Select a.FirstName+' ' +a.LastName  as 'Author',  Max (b.Price) as 'Price'
+From Books b
+join Authors a on b.Id_Author=a.Id_Author
+Group by a.FirstName+' ' +a.LastName 
+) groupeda
+on b.Price=groupeda.Price
+and a.FirstName+' '+a.LastName=groupeda.Author
+Group by  b.NameBook,a.FirstName+' ' +a.LastName,b.Price 
+
+Select * from AuthorMostExpansiveBooks
+Order by Price desc
+
 --9. Написать представление, которое позволяет вывести всю информацию о
 --работе магазинов. Отсортировать выборку по странам в возрастающем и
 --по названиям магазинов в убывающем порядке.
+
+
 --10.Написать зашифрованное представление, показывающее самую
 --популярную книгу.
 --11. Написать модифицированное представление, в котором предоставляется
